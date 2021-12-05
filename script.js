f6bd92e493ab059349ca4273f234409083b33764
yungsasha777 <yungsasha777@gmail.com>
2021-12-05 13:23:50 +0300

    fixed script

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var xhttp = new XMLHttpRequest();
 let result = 'empty';
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    fetchJson(JSON.parse(this.responseText));
+    //fetchJson(JSON.parse(this.responseText));
+    fetchJson(this.responseText);
   };
 };
 xhttp.open("GET", "https://raw.githubusercontent.com/yungsasha777/21.11.21/main/db.json", true);
