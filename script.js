8c94e72be49379477b7674820bf65cae1d8f2ed1
yungsasha777 <yungsasha777@gmail.com>
2021-12-26 13:03:35 +0300

    fr

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ xhttp.onreadystatechange = function() {
 xhttp.open("GET", "https://raw.githubusercontent.com/yungsasha777/21.11.21/main/db.json", true);
 xhttp.send();
 
+const cart = document.getElementById('cartContainer');
+let cartList = [];
+
 function fetchJson(arg, arg2) {
     result = arg;
     console.log(result.products);
@@ -15,14 +18,39 @@ function fetchJson(arg, arg2) {
 };
 
 function constructPage(item, index, arr) {
-    let contentToAppend = `<div class="container">
+    let contentToAppend = `<div class="container" id="item${index}">
   <h1 class="name">${item.name}</h1>
   <h3 class="name">Description:</h3>
   <p style="margin-left: 20px">${JSON.stringify(item.description).replace(/"|{|}/gi, ' ').replace(/ , /gi, "; ").replace('[', ' ').replace(']', ' ')}</p>
   <h3 class="name">Seller: ${item.seller}</h3>
   <h3 class="name">Price: ${item.price}</h3>
+  <button type="button" id="item${index}button" onclick="addToCart(${index})">Buy</button>
 </div>`;
 
     $('#maincontainer').append(contentToAppend)
 
+}
+
+function openCart() {
+    cart.classList.toggle('hide');
+};
+
+function addToCart(id){
+    let elem = document.getElementById(`item${id}`);
+    cart.append(elem);
+    elem.style.marginLeft = '15px';
+    elem.style.marginTop = '15px';
+    document.getElementById(`item${id}button`).innerText = "Remove";
+    document.getElementById(`item${id}button`).setAttribute("onclick", `removeFromCart(${id})`);
+    cartList.unshift(`item${id}`);
+};
+
+function removeFromCart(id){
+    let parent = document.getElementById('maincontainer');
+    let elem = document.getElementById(`item${id}`);
+    parent.prepend(elem);
+    elem.style.marginLeft = '0px';
+    elem.style.marginTop = '0px';
+    document.getElementById(`item${id}button`).innerText = "Buy";
+    document.getElementById(`item${id}button`).setAttribute("onclick", `addToCart(${id})`);
 }
